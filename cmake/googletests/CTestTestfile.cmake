4063ce976997c80901cbd3c62a51edc0d5937e73
steppans <steppans@mail.ru>
2022-04-22 11:37:42 +0300

    deleted builds for cmake

--- cmake/googletests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/steppans/GITHUB_labs/lab05/googletests
-# Build directory: /home/steppans/GITHUB_labs/lab05/cmake/googletests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
